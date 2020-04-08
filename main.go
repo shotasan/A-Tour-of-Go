339fbc9f35d56b387e362a03b495fc4aff0b62c0
shotasan <wondermoai@gmail.com>
2020-04-08 20:17:31 +0900

    Swith

--- main.go
+++ main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-func pow(x, n, lim float64) float64 {
-	// ifで評価する前に変数宣言が可能
-	if v := math.Pow(x, n); v < lim {
-		return v
-		// else内でもvは利用可能
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	// スコープを外れるのでvは使えない
-	return lim
-}
-
 func main() {
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 20),
-	)
+	// caseの最後にbreakは不要
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s", os)
+	}
 }
