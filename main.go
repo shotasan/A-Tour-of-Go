9a2047370096b00a6720aba2e20a361ae8e9a06e
shotasan <wondermoai@gmail.com>
2020-04-08 20:21:52 +0900

    Defer

--- main.go
+++ main.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
+// deferはすぐに評価されるが呼び出し元の関数がreturnするまで実行されない
 func main() {
-	// caseの最後にbreakは不要
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X")
-	case "linux":
-		fmt.Println("Linux")
-	default:
-		fmt.Printf("%s", os)
-	}
+	defer fmt.Println("wrold")
+
+	fmt.Println("hello")
 }
