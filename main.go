416af866445382ef51e4dbcdd0d68583c1538f9a
shotasan <wondermoai@gmail.com>
2020-04-09 21:36:05 +0900

    Pointers to Structs

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ type Vertex struct {
 
 func main() {
 	v := Vertex{1, 2}
-	v.X = 4
-	fmt.Println(v.X)
+	p := &v             //構造体のポインタの取得
+	fmt.Println((*p).X) //ポインタを通して構造体の値にアクセス
+	fmt.Println(p.X)    //省略しても書ける
+	p.X = 1e9
+	fmt.Println(v)
 }
