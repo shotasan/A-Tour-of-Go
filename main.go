3f61b84c59cfcc8830cbb313f79c0f111d4f6136
shotasan <wondermoai@gmail.com>
2020-04-09 21:24:41 +0900

    Structs

--- main.go
+++ main.go
@@ -4,20 +4,12 @@ import (
 	"fmt"
 )
 
+//Vertex : hoge
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
-	i, j := 42, 2701
-
-	p := &i         //&でポインタをしゅとくする
-	fmt.Println(p)  //ポインタ
-	fmt.Println(*p) //ポインタを通して値を取得
-	*p = 21         //ポインタを通して値を更新
-	fmt.Println(p)
-	fmt.Println(*p)
-	fmt.Println(i)
-
-	p = &j
-	*p = *p / 37
-	fmt.Println(p)
-	fmt.Println(*p)
-	fmt.Println(j)
+	fmt.Println(Vertex{10, 2})
 }
