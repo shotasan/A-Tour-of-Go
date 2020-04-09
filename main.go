cff81370ace1f877881448e9685feacf52e728a5
shotasan <wondermoai@gmail.com>
2020-04-09 22:09:14 +0900

    Slices are like references to arrays

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 )
 
-//Vertex : hoge
-type Vertex struct {
-	X int
-	Y int
-}
-
 func main() {
-	// 要素数６の配列を宣言
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	names := [4]string{
+		"John", "Paul", "George", "Ringo",
+	}
+	fmt.Println(names)
 
-	// sはスライス
-	// 変数[low:high]でlowからhighの一つ前を含むスライスを作る
-	var s []int = primes[1:4]
-	var m []int = primes[0:1]
-	fmt.Println(s)
-	fmt.Println(m)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	// スライスの要素を変更すると、元となる配列の対応する要素が変更される。
+	// 同じ元となる配列を共有している他のスライスは、それらの変更が反映される。
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
