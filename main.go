88d5684294a97da77b416f860075b5b5f823b182
shotasan <wondermoai@gmail.com>
2020-04-09 21:55:45 +0900

    Slices

--- main.go
+++ main.go
@@ -11,14 +11,13 @@ type Vertex struct {
 }
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
-
-	// 配列の長さより要素数が少ない場合は０が入る。多い場合はコンパイルエラー
-	// primes := [6]int{2, 3, 5, 7, 11} //=> [2 3 5 7 11 0]
+	// 要素数６の配列を宣言
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
+
+	// sはスライス
+	// 変数[low:high]でlowからhighの一つ前を含むスライスを作る
+	var s []int = primes[1:4]
+	var m []int = primes[0:1]
+	fmt.Println(s)
+	fmt.Println(m)
 }
