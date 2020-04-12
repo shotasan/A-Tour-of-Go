e7a25322e4251a947eabf529dec4024b5febf5d5
shotasan <wondermoai@gmail.com>
2020-04-12 22:44:08 +0900

    Slice length and capacity

--- main.go
+++ main.go
@@ -6,21 +6,18 @@ import (
 
 func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	// 配列同様、インデックスでアクセスできる
-	fmt.Println(s[0])
+	s = s[:0]
+	printSlice(s)
 
-	// 通常のスライス
-	s = s[1:4]
-	fmt.Println(s)
+	s = s[:3]
+	printSlice(s)
 
-	// 下限値を省略した記述
-	// 下限値の既定値は０なので[0:2]と等価
-	s = s[:2]
-	fmt.Println(s)
+	s = s[2:]
+	printSlice(s)
+}
 
-	// 上限値を省略した記述
-	// 上限値の既定値はスライスの長さ
-	s = s[1:]
-	fmt.Println(s)
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
